c8a06dd58a04f64c16d7ad13e1cca57f776c4c30
John Williamson <johnjwilliamson@live.com>
2021-07-29 14:50:27 +1000

    fix svg

--- src/elements/insight/Needle.tsx
+++ src/elements/insight/Needle.tsx
@@ -12,10 +12,10 @@ export const Needle = ({ percent }: { percent: number }) => {
         r="31.8"
         fill="none"
         stroke="red"
-        stroke-width="1"
-        stroke-dasharray="39.5 200"
-        stroke-dashoffset="-98"
-        stroke-linecap="round"
+        strokeWidth="1"
+        strokeDasharray="39.5 200"
+        strokeDashoffset="-98"
+        strokeLinecap="round"
       />
       <circle
         cx="34"
@@ -23,10 +23,10 @@ export const Needle = ({ percent }: { percent: number }) => {
         r="31.8"
         fill="none"
         stroke="lightgray"
-        stroke-width="1"
-        stroke-dasharray="20 200"
-        stroke-dashoffset="-140"
-        stroke-linecap="round"
+        strokeWidth="1"
+        strokeDasharray="20 200"
+        strokeDashoffset="-140"
+        strokeLinecap="round"
       />
       <circle
         cx="34"
@@ -34,10 +34,10 @@ export const Needle = ({ percent }: { percent: number }) => {
         r="31.8"
         fill="none"
         stroke="green"
-        stroke-width="1"
-        stroke-dasharray="39 200"
-        stroke-dashoffset="-98"
-        stroke-linecap="round"
+        strokeWidth="1"
+        strokeDasharray="39 200"
+        strokeDashoffset="-98"
+        strokeLinecap="round"
         transform="scale(-1 1)"
         transform-origin="34 34"
       />
@@ -48,8 +48,8 @@ export const Needle = ({ percent }: { percent: number }) => {
         x2="34"
         y2="34"
         stroke="black"
-        stroke-width="0.5"
-        stroke-linecap="round"
+        strokeWidth="0.5"
+        strokeLinecap="round"
         transform={`rotate(${rotation})`}
         transform-origin="34 34"
       />
@@ -59,13 +59,13 @@ export const Needle = ({ percent }: { percent: number }) => {
         r="1.5"
         fill="white"
         stroke="black"
-        stroke-width="0.5"
+        strokeWidth="0.5"
       />
 
       <defs>
         <linearGradient id="meter-bg" gradientTransform="rotate(90)">
-          <stop offset="0%" stop-color={gradientColour} stop-opacity="90%" />
-          <stop offset="55%" stop-color="white" stop-opacity="0%" />
+          <stop offset="0%" stopColor={gradientColour} stopOpacity="90%" />
+          <stop offset="55%" stopColor="white" stopOpacity="0%" />
         </linearGradient>
       </defs>
     </svg>
