91758bce22915695d8b40f7364cf2ff993dd4034
Doron Zavelevsky <zavelevsky@gmail.com>
2021-10-07 11:48:45 +0300

    pools table - feedback fixes

--- src/elements/earn/pools/poolsTable/PoolsTableCellApr.tsx
+++ src/elements/earn/pools/poolsTable/PoolsTableCellApr.tsx
@@ -1,13 +1,12 @@
 import { Pool } from 'services/observables/tokens';
 
 export const PoolsTableCellApr = (pool: Pool) => {
-  const aprOne = pool.reserves[0].rewardApr;
-  const aprTwo = pool.reserves[1].rewardApr;
+  const aprOne = pool.reserves[0].rewardApr || 0;
+  const aprTwo = pool.reserves[1].rewardApr || 0;
   const symbolOne = pool.reserves[0].symbol;
   const symbolTwo = pool.reserves[1].symbol;
-  const ends_at = pool.reward?.ends_at;
   const formatApr = (rewardApr: number) => (rewardApr + pool.apr).toFixed(2);
-  return aprOne && aprTwo && ends_at ? (
+  return (
     <div className="flex items-center w-full">
       <div className="flex justify-start w-full">
         <span className="">{`${symbolTwo} ${formatApr(aprTwo)}%`}</span>
@@ -15,7 +14,5 @@ export const PoolsTableCellApr = (pool: Pool) => {
         <span className="">{`${symbolOne} ${formatApr(aprOne)}%`}</span>
       </div>
     </div>
-  ) : (
-    ''
   );
 };
