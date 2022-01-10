1f1d20168f2a3e896c2406acee90b3d06c33856e
Jan Langheimer <langheimer@gmail.com>
2022-01-10 16:13:52 +0100

    testing: add new pools selector to pool table

---
+++ src/redux/bancor2/pools.selector.ts
@@ -0,0 +1,106 @@
+import { createSelector } from '@reduxjs/toolkit';
+import { RootState } from '../index';
+import { APIPool } from '../../services/api/bancor';
+import { Pool, Reserve, Token } from '../../services/observables/tokens';
+import { Address } from '../../services/web3/types';
+import { getAllTokensByTL } from './tokens.selector';
+import BigNumber from 'bignumber.js';
+
+interface BuildPoolProps {
+  apiPool: APIPool;
+  tkn: Token;
+  bnt: Token;
+}
+
+// Helper function to build pool object
+const buildPool = ({ apiPool, tkn, bnt }: BuildPoolProps): Pool | undefined => {
+  const liquidity = Number(apiPool.liquidity.usd ?? 0);
+  const fees_24h = Number(apiPool.fees_24h.usd ?? 0);
+  let apr = 0;
+  if (liquidity && fees_24h) {
+    apr = new BigNumber(fees_24h)
+      .times(365)
+      .div(liquidity)
+      .times(100)
+      .toNumber();
+  }
+
+  const reserveTkn = apiPool.reserves.find(
+    (r) => r.address.toLowerCase() === tkn.address.toLowerCase()
+  );
+  if (!reserveTkn) {
+    return undefined;
+  }
+  const reserveBnt = apiPool.reserves.find(
+    (r) => r.address.toLowerCase() === bnt.address.toLowerCase()
+  );
+  if (!reserveBnt) {
+    return undefined;
+  }
+
+  const reserves: Reserve[] = [
+    {
+      ...reserveTkn,
+      rewardApr: Number(reserveTkn.apr) / 10000,
+      symbol: tkn.symbol,
+      logoURI: tkn.logoURI,
+      decimals: tkn.decimals,
+      usdPrice: tkn.usdPrice,
+    },
+    {
+      ...reserveBnt,
+      rewardApr: Number(reserveBnt.apr) / 10000,
+      symbol: bnt.symbol,
+      logoURI: bnt.logoURI,
+      decimals: bnt.decimals,
+      usdPrice: bnt.usdPrice,
+    },
+  ];
+
+  return {
+    name: apiPool.name,
+    pool_dlt_id: apiPool.pool_dlt_id,
+    converter_dlt_id: apiPool.converter_dlt_id,
+    reserves,
+    liquidity,
+    volume_24h: Number(apiPool.volume_24h.usd ?? 0),
+    fees_24h,
+    fee: Number(apiPool.fee) / 10000,
+    version: apiPool.version,
+    supply: Number(apiPool.supply),
+    decimals: apiPool.decimals,
+    isWhitelisted: apiPool.isWhitelisted,
+    apr,
+    reward: apiPool.reward,
+    isProtected: apiPool.isWhitelisted, // TODO add isProtected by fetching minNetworkTokenLiquidityForMinting
+  };
+};
+
+export const getAllPoolsByTL = createSelector(
+  [
+    (state: RootState) => state.apiData.apiPools,
+    (state: RootState) => getAllTokensByTL(state),
+  ],
+  (apiPools: Map<Address, APIPool>, tokens: Token[]): Pool[] => {
+    const apiPoolsArray = Array.from(apiPools.values());
+    const bnt = tokens.find((t) => t.symbol === 'BNT');
+    if (!bnt) {
+      return [];
+    }
+    const pools = tokens.map((tkn) => {
+      if (tkn.symbol === 'BNT') {
+        return undefined;
+      }
+      const apiPool = apiPoolsArray.find((p) =>
+        p.reserves.find(
+          (r) => r.address.toLowerCase() === tkn.address.toLowerCase()
+        )
+      );
+      if (!apiPool) {
+        return undefined;
+      }
+      return buildPool({ apiPool, tkn, bnt });
+    });
+    return pools.filter((pool) => !!pool) as Pool[];
+  }
+);
